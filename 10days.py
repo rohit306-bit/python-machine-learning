a9269b6d83d0b5dbdc5e2f76b56bfff7261d2d2c
ROHIT RAJAT <76672224+rohit306-bit@users.noreply.github.com>
2021-10-26 20:50:22 +0530

    Add files via upload

---
+++ 10days.py
@@ -0,0 +1,36 @@
+#Linear Regression:-  supervised learning
+
+#k-mean Clustring :- unsupervised learning
+#eculidian distance
+#linear distance
+#cartesian distance
+#spatial distance
+# there are four coordinates in space which is x,y,z and time, it is knows as euclidian.
+#Euclidian is a distance between two vector
+#vector having more than one values
+#scalar having single values which is magnitude
+'''
+X       Y        c1(2,0)      c2(5,1)          c3(1,4)
+1        1         2            4                  3      (c1)    
+2        0         0            4                  5      (c1)
+4        0         2             2                 7       (c2)
+5        1         4             0                  7      (c2)
+0        5         7              9                 2       (c3)
+1        4         5              7                 0       (c3)
+
+
+step 1 :- randomanly choose any no. of cluster
+step 2 :- choose k no. of cluster such c1,c2,c3. take c1, c2,c3 values random as your wish.
+
+'''
+
+'''
+
+1. UNSUPERVISED LEARNING
+2. SUPERVISED LEARNING
+3. REINFORCEMENT LEARNING
+4. DEEP LEARNING
+
+
+
+'''
